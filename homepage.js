ad6a5320dfb0b5de5f09a8a26dca0f6ecc931128
smuchara <68949651+smuchara@users.noreply.github.com>
2022-01-17 17:17:28 +0300

    update19

--- homepage.js
+++ homepage.js
@@ -6,5 +6,7 @@ function openNav() {
     document.getElementById("navbar").style.width = "0";
   }
   function opencontacts(){
-    document.getElementById("more").style.height = "450px"
+    document.getElementById("more").style.height = "450px";
+    document.getElementById("btno").style.backgroundColor = "rgba(255, 255, 255, 0)"
+    document.getElementById("btno").style.color="rgba(255, 255, 255, 0)"
   }
