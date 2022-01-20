f29dd44b894de89073f1aff2ac9e3e6b3aae80fc
smuchara <68949651+smuchara@users.noreply.github.com>
2022-01-20 15:07:19 +0300

    animationupdate1

--- homepage.js
+++ homepage.js
@@ -5,8 +5,30 @@ function openNav() {
   function closeNav() {
     document.getElementById("navbar").style.width = "0";
   }
+
   function opencontacts(){
-    document.getElementById("more").style.height = "450px";
-    document.getElementById("btno").style.backgroundColor = "rgba(255, 255, 255, 0)"
-    document.getElementById("btno").style.color="rgba(255, 255, 255, 0)"
+    var btn2 =  document.getElementById("btn2");
+    var btn3 = document.getElementById("btn3");
+    var btn4 = document.getElementById("btn4");
+    
+    //design for btn2//
+    btn2.style.marginBottom = "60px"
+    btn2.style.color = "black";
+    btn2.style.backgroundColor = "white"
+
+     //design for btn3//
+    btn3.style.transform = "translateY(-60px)";
+    btn3.style.transition = "ease-in";
+    btn3.style.transitionDuration = "0.7s";
+    btn3.style.transitionDelay = "0.5s";
+    btn3.style.color = "black";
+    btn3.style.backgroundColor = "white"
+
+    //design for btn4//
+    btn4.style.transform = "translateY(-60px)";
+    btn4.style.transition = "ease-in";
+    btn4.style.transitionDuration = "0.7s";
+    btn4.style.transitionDelay = "1s";
+    btn4.style.color = "black";
+    btn4.style.backgroundColor = "white"
   }
