6f2acefe7876ddde2573946bdddefbcd07bce640
smuchara <68949651+smuchara@users.noreply.github.com>
2022-01-21 18:02:17 +0300

    animationupdatefinal

--- homepage.js
+++ homepage.js
@@ -14,34 +14,34 @@ function opencontacts() {
   var btn5 = document.getElementById("btn5");
 
   //design for btn2//
-  btn2.style.transform = "translateY(-60px)";
+  //btn2.style.transform = "translateY(-60px)";//
   btn2.style.transition = "ease-in";
   btn2.style.transitionDuration = "0.7s";
-  btn2.style.transitionDelay = "0.5s";
+  btn2.style.transitionDelay = "0.3s";
   btn2.style.color = "black";
   btn2.style.backgroundColor = "white"
 
   //design for btn3//
-  btn3.style.transform = "translateY(-60px)";
+  //btn3.style.transform = "translateY(-60px)";//
   btn3.style.transition = "ease-in";
   btn3.style.transitionDuration = "0.7s";
-  btn3.style.transitionDelay = "1s";
+  btn3.style.transitionDelay = "0.5s";
   btn3.style.color = "black";
   btn3.style.backgroundColor = "white"
 
   //design for btn4//
-  btn4.style.transform = "translateY(-60px)";
+  //btn4.style.transform = "translateY(-60px)";//
   btn4.style.transition = "ease-in";
-  btn4.style.transitionDuration = "0.7s";
-  btn4.style.transitionDelay = "1.5s";
+  btn4.style.transitionDuration = "0.5s";
+  btn4.style.transitionDelay = "0.7s";
   btn4.style.color = "black";
   btn4.style.backgroundColor = "white"
 
   //design for btn5//
-  btn5.style.transform = "translateY(-60px)";
+  //btn5.style.transform = "translateY(-60px)";//
   btn5.style.transition = "ease-in";
-  btn5.style.transitionDuration = "0.7s";
-  btn5.style.transitionDelay = "2s";
+  btn5.style.transitionDuration = "0.5s";
+  btn5.style.transitionDelay = "0.8s";
   btn5.style.color = "black";
   btn5.style.backgroundColor = "white";
 
@@ -55,35 +55,43 @@ function closeContacts() {
   var btn4 = document.getElementById("btn4");
   var btn5 = document.getElementById("btn5");
   //design for btn2//
-  btn2.style.transform = "translateY(0px)";
-  btn2.style.transition = "ease-in";
+  //btn2.style.transform = "translateY(0px)";//
+  btn2.style.transition = "ease-out";
   btn2.style.transitionDuration = "0.7s";
   btn2.style.transitionDelay = "0.3s";
   btn2.style.color = "black";
   btn2.style.backgroundColor = "white"
+  btn2.style.backgroundColor = "Transparent"
+  btn2.style.color = "Transparent"
 
   //design for btn3//
-  btn3.style.transform = "translateY(60px)";
-  btn3.style.transition = "ease-in";
+  //btn3.style.transform = "translateY(60px)";//
+  btn3.style.transition = "ease-out";
   btn3.style.transitionDuration = "0.7s";
   btn3.style.transitionDelay = "0.5s";
   btn3.style.color = "black";
   btn3.style.backgroundColor = "white"
+  btn3.style.backgroundColor = "Transparent"
+  btn3.style.color = "Transparent"
 
   //design for btn4//
-  btn4.style.transform = "translateY(120px)";
-  btn4.style.transition = "ease-in";
+  //btn4.style.transform = "translateY(120px)";//
+  btn4.style.transition = "ease-out";
   btn4.style.transitionDuration = "0.5s";
   btn4.style.transitionDelay = "0.7s";
   btn4.style.color = "black";
   btn4.style.backgroundColor = "white"
+  btn4.style.backgroundColor = "Transparent"
+  btn4.style.color = "Transparent"
 
   //design for btn5//
-  btn5.style.transform = "translateY(180px)";
-  btn5.style.transition = "ease-in";
+  //btn5.style.transform = "translateY(180px)";//
+  btn5.style.transition = "ease-out";
   btn5.style.transitionDuration = "0.5s";
   btn5.style.transitionDelay = "0.8s";
   btn5.style.color = "black";
+  btn5.style.backgroundColor = "Transparent"
+  btn5.style.color = "Transparent"
 
 
   btn1.style.display = "block";
